7549a7a0455fb2713a47616eb3b0c0bb22c7f5db
Haudekozaur <julian.haudek@gmail.com>
2023-04-04 19:54:20 +0200

    Extracting athletes names

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello from git and venv!")
+import requests
+from bs4 import BeautifulSoup
+
+
+class Scraper():
+
+    def scrapowanie():
+        print("scrapu, scrapu")
+
+
+requested_html_text = requests.get(
+    'https://hmps.domtel-sport.pl/?seria=3&dzien=2023-02-19&runda=1&konkurencja=M200&impreza=6')
+requested_html_text.encoding = 'utf-8'
+html_text = requested_html_text.text
+# with open('HMP_2023.html', 'r', encoding='utf-8') as html_file:
+soup = BeautifulSoup(html_text, 'lxml')
+pretty_soup = soup.prettify()
+
+rows = soup.find_all('a', href=True, onclick=True)
+
+for row in rows:
+    print(row.text)
