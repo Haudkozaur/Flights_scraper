36a13eb1cb206f64164cfd448f98f4ff476401fb
Haudekozaur <julian.haudek@gmail.com>
2023-04-06 13:48:34 +0200

    Extracting scoreboard from the Domtel website

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import requests
-from bs4 import BeautifulSoup
+from Table_class import Table
+
+requested_html = requests.get(
+    'https://hmps.domtel-sport.pl/?seria=0&dzien=2023-02-19&runda=3&konkurencja=M200&impreza=6')
 
 
-class Scraper():
+event_results = Table(requested_html)
+event_results.get_headers()
+event_results.get_rows()
 
-    def scrapowanie():
-        print("scrapu, scrapu")
 
 
-requested_html_text = requests.get(
-    'https://hmps.domtel-sport.pl/?seria=3&dzien=2023-02-19&runda=1&konkurencja=M200&impreza=6')
-requested_html_text.encoding = 'utf-8'
-html_text = requested_html_text.text
-# with open('HMP_2023.html', 'r', encoding='utf-8') as html_file:
-soup = BeautifulSoup(html_text, 'lxml')
-pretty_soup = soup.prettify()
 
-rows = soup.find_all('a', href=True, onclick=True)
-
-for row in rows:
-    print(row.text)
