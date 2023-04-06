c6bc1420f11c920fadabc1baabf1cd3ac62a9ad2
Haudekozaur <julian.haudek@gmail.com>
2023-04-06 19:24:57 +0200

    Added simple GUI

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import requests
 from Table_class import Table
 
 requested_html = requests.get(
-    'https://hmps.domtel-sport.pl/?seria=0&dzien=2023-02-19&runda=3&konkurencja=M200&impreza=6')
+    'https://wmaci2023.domtel-sport.pl/?seria=0&runda=3&konkurencja=MTJ_35&dzien=&impreza=6')
 
 
 event_results = Table(requested_html)
 event_results.get_headers()
 event_results.get_rows()
-
+event_results.display_table()
 
 
 
